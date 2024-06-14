21bfc0ae8e3df79d002be8c1cace1bb9b11aa6dc
jahidinn <jahidin.inspirit@gmail.com>
2024-06-15 01:22:41 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,8 @@ use GuzzleHttp\Promise\Create;
 |
 */
 
-
 Route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
 Route::post('/login', [AuthController::class, 'autenticate']);
-
 Route::get('/register', [AuthController::class, 'register'])->name('register')->middleware('guest');
 Route::post('/register', [AuthController::class, 'storeRegister']);
 
@@ -36,7 +34,6 @@ Route::post('/register', [AuthController::class, 'storeRegister']);
 Route::post('/subscribe', [HomeController::class, 'subscribe']);
 # Contact us
 Route::post('/send-message', [HomeController::class, 'sendMessage']);
-
 Route::post('/logout', [AuthController::class, 'logout']);
 
 //email verification handle
@@ -47,30 +44,80 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/search', [HomeController::class, 'searchEvent']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
-Route::get('/dashboard/myevent', [DashboardController::class, 'myEvent'])->middleware('auth');
-Route::get('/dashboard/get-myevent', [DashboardController::class, 'getMyEvent'])->middleware('auth');
-Route::get('/dashboard/manajemen-event', [DashboardController::class, 'manajemenEvent'])->middleware('auth');
-Route::get('/dashboard/get-customform', [DashboardController::class, 'getCustomformParticipant'])->middleware('auth');
-Route::post('/dashboard/delete-myevent', [DashboardController::class, 'deleteMyevent'])->middleware('auth');
-
-Route::get('/dashboard/participant-data', [DashboardController::class, 'participant'])->middleware('auth');
-Route::get('/dashboard/get-participant', [DashboardController::class, 'getParticipant'])->middleware('auth');
-Route::get('/dashboard/participant-download-excel/{id}', [DashboardController::class, 'downloadExcel'])->middleware('auth');
-
-Route::get('/dashboard/event-checkin', [DashboardController::class, 'eventCheckin'])->middleware('auth');
-Route::get('/dashboard/get-participan-checkin', [DashboardController::class, 'getParticipantCheckin'])->middleware('auth');
-Route::post('/dashboard/participant-checkin', [DashboardController::class, 'checkinProcess'])->middleware('auth');
-
-Route::get('/dashboard/transaction-report', [DashboardController::class, 'transactionReport'])->middleware('auth');
-Route::get('/dashboard/get-transaction-report', [DashboardController::class, 'getTransactionReport'])->middleware('auth');
-Route::get('/dashboard/check-event-date', [DashboardController::class, 'checkEventDate'])->middleware('auth');
-Route::post('/dashboard/withdraw-process', [DashboardController::class, 'withdraw'])->middleware('auth');
-Route::get('/dashboard/withdraw-history', [DashboardController::class, 'withdrawHistory'])->middleware('auth');
-
 Route::middleware(['auth'])->group(function () {
 	// Rute-rute yang akan terkena middleware auth
 	// LIST ROUTE ......
+
+	# event
+	Route::post('/event-edit-image', [EventController::class, 'editImage']);
+	Route::post('/event-edit', [EventController::class, 'editProcess']);
+	Route::resource('/event', EventController::class, ['except' => ['show']]);
+
+	Route::get('/get-cities/{code}', [EventController::class, 'getCities']);
+	Route::get('/check-url', [EventController::class, 'cekUrl']);
+	Route::get('/get-my-org', [EventController::class, 'getMyOrg']);
+
+	Route::get('/get-ticket', [EventController::class, 'getTicket']);
+	Route::get('/get-formulir', [EventController::class, 'getFormulir']);
+
+	Route::post('/add-ticket', [EventController::class, 'addTicket']);
+	Route::post('/add-formulir', [EventController::class, 'addFormulir']);
+
+	//Route::get('/check-ticket-participant', [EventController::class, 'checkTicketParticipant']);
+	Route::post('/edit-ticket', [EventController::class, 'editTicket']);
+	Route::post('/edit-formulir', [EventController::class, 'editFormulir']);
+
+	Route::post('/delete-ticket', [EventController::class, 'deleteTicket']);
+	Route::post('/delete-formulir', [EventController::class, 'deleteFormulir']);
+
+
+	# Dashboard
+	Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+	Route::get('/dashboard/myevent', [DashboardController::class, 'myEvent']);
+	Route::get('/dashboard/get-myevent', [DashboardController::class, 'getMyEvent']);
+	Route::get('/dashboard/manajemen-event', [DashboardController::class, 'manajemenEvent']);
+	Route::get('/dashboard/get-customform', [DashboardController::class, 'getCustomformParticipant']);
+	Route::post('/dashboard/delete-myevent', [DashboardController::class, 'deleteMyevent']);
+
+	Route::get('/dashboard/participant-data', [DashboardController::class, 'participant']);
+	Route::get('/dashboard/get-participant', [DashboardController::class, 'getParticipant']);
+	Route::get('/dashboard/participant-download-excel/{id}', [DashboardController::class, 'downloadExcel']);
+
+	Route::get('/dashboard/event-checkin', [DashboardController::class, 'eventCheckin']);
+	Route::get('/dashboard/get-participan-checkin', [DashboardController::class, 'getParticipantCheckin']);
+	Route::post('/dashboard/participant-checkin', [DashboardController::class, 'checkinProcess']);
+
+	Route::get('/dashboard/transaction-report', [DashboardController::class, 'transactionReport']);
+	Route::get('/dashboard/get-transaction-report', [DashboardController::class, 'getTransactionReport']);
+	Route::get('/dashboard/check-event-date', [DashboardController::class, 'checkEventDate']);
+	Route::post('/dashboard/withdraw-process', [DashboardController::class, 'withdraw']);
+	Route::get('/dashboard/withdraw-history', [DashboardController::class, 'withdrawHistory']);
+
+	Route::get('/dashboard/article', [DashboardController::class, 'article']);
+	Route::get('/dashboard/article/get', [DashboardController::class, 'getArticle']);
+
+	Route::get('/dashboard/my-profile', [UserProfileController::class, 'index']);
+	Route::post('/dashboard/edit-profile-image', [UserProfileController::class, 'editImage']);
+	Route::post('/dashboard/edit-password', [UserProfileController::class, 'editPassword']);
+	Route::get('/dashboard/get-data-profile', [UserProfileController::class, 'getMyProfile']);
+	Route::post('/dashboard/edit-profile-process', [UserProfileController::class, 'editProfile']);
+
+	Route::get('/dashboard/organization', [OrganizationController::class, 'index']);
+	Route::post('/dashboard/add-organization', [OrganizationController::class, 'createOrg']);
+	Route::post('/dashboard/edit-organization', [OrganizationController::class, 'editOrg']);
+	Route::get('/dashboard/get-myorganization', [OrganizationController::class, 'getMyOrg']);
+	Route::get('/dashboard/detail-organization', [OrganizationController::class, 'detailOrg']);
+	Route::post('/dashboard/delete-organization', [OrganizationController::class, 'deleteOrg']);
+	Route::get('/dashboard/get-organization', [OrganizationController::class, 'getOrg']);
+
+	Route::post('/dashboard/follow-organization', [OrganizationController::class, 'followOrg']);
+	Route::post('/dashboard/unfollow-organization', [OrganizationController::class, 'unfollowOrg']);
+	Route::post('/dashboard/accept-follow', [OrganizationController::class, 'AcceptFollow']);
+	Route::post('/dashboard/remove-member', [OrganizationController::class, 'removeMember']);
+	Route::get('/dashboard/get-foll-organization', [OrganizationController::class, 'myFollowingOrg']);
+
+	Route::get('/dashboard/get-organization-member', [OrganizationController::class, 'getOrgMember']);
+	Route::get('/dashboard/get-organization-request', [OrganizationController::class, 'getOrgMemberReequest']);
 
 	# Artikel
 	Route::post('/administrator/article/post', [ArticleController::class, 'create']);
@@ -148,60 +195,10 @@ Route::get('/blog', [ArticleController::class, 'blogMain']);
 Route::get('/blog/search', [ArticleController::class, 'blogSearch']);
 Route::get('/blog/{slug}', [ArticleController::class, 'viewArticle']);
 
-Route::get('/dashboard/article', [DashboardController::class, 'article'])->middleware('auth');
-Route::get('/dashboard/article/get', [DashboardController::class, 'getArticle'])->middleware('auth');
-
-Route::get('/dashboard/my-profile', [UserProfileController::class, 'index'])->middleware('auth');
-Route::post('/dashboard/edit-profile-image', [UserProfileController::class, 'editImage'])->middleware('auth');
-Route::post('/dashboard/edit-password', [UserProfileController::class, 'editPassword'])->middleware('auth');
-Route::get('/dashboard/get-data-profile', [UserProfileController::class, 'getMyProfile'])->middleware('auth');
-Route::post('/dashboard/edit-profile-process', [UserProfileController::class, 'editProfile'])->middleware('auth');
-
-Route::get('/dashboard/organization', [OrganizationController::class, 'index'])->middleware('auth');
-Route::post('/dashboard/add-organization', [OrganizationController::class, 'createOrg'])->middleware('auth');
-Route::post('/dashboard/edit-organization', [OrganizationController::class, 'editOrg'])->middleware('auth');
-Route::get('/dashboard/get-myorganization', [OrganizationController::class, 'getMyOrg'])->middleware('auth');
-Route::get('/dashboard/detail-organization', [OrganizationController::class, 'detailOrg'])->middleware('auth');
-Route::post('/dashboard/delete-organization', [OrganizationController::class, 'deleteOrg'])->middleware('auth');
-Route::get('/dashboard/get-organization', [OrganizationController::class, 'getOrg'])->middleware('auth');
-
-Route::post('/dashboard/follow-organization', [OrganizationController::class, 'followOrg'])->middleware('auth');
-Route::post('/dashboard/unfollow-organization', [OrganizationController::class, 'unfollowOrg'])->middleware('auth');
-Route::post('/dashboard/accept-follow', [OrganizationController::class, 'AcceptFollow'])->middleware('auth');
-Route::post('/dashboard/remove-member', [OrganizationController::class, 'removeMember'])->middleware('auth');
-Route::get('/dashboard/get-foll-organization', [OrganizationController::class, 'myFollowingOrg'])->middleware('auth');
-
-Route::get('/dashboard/get-organization-member', [OrganizationController::class, 'getOrgMember'])->middleware('auth');
-Route::get('/dashboard/get-organization-request', [OrganizationController::class, 'getOrgMemberReequest'])->middleware('auth');
-
 Route::get('/organisasi/{organisasi}', [OrganizationController::class, 'detailOrganisasi']);
 Route::get('/user/{username}', [UserProfileController::class, 'userPublicInfo']);
 
-Route::get('/get-cities/{code}', [EventController::class, 'getCities']);
-Route::get('/check-url', [EventController::class, 'cekUrl']);
-Route::get('/get-my-org', [EventController::class, 'getMyOrg']);
-
-Route::get('/get-ticket', [EventController::class, 'getTicket']);
-Route::get('/get-formulir', [EventController::class, 'getFormulir']);
-
-Route::post('/add-ticket', [EventController::class, 'addTicket']);
-Route::post('/add-formulir', [EventController::class, 'addFormulir']);
-
-Route::get('/check-ticket-participant', [EventController::class, 'checkTicketParticipant']);
-Route::post('/edit-ticket', [EventController::class, 'editTicket']);
-Route::post('/edit-formulir', [EventController::class, 'editFormulir']);
-
-Route::post('/delete-ticket', [EventController::class, 'deleteTicket']);
-Route::post('/delete-formulir', [EventController::class, 'deleteFormulir']);
-
-Route::post('/event-edit-image', [EventController::class, 'editImage']);
-
-Route::post('/event-edit', [EventController::class, 'editProcess'])->middleware('auth');
-//detail event
-Route::resource('/event', EventController::class, ['except' => ['show']])->middleware('auth');
-
-
-//Checkout
+# Checkout
 Route::get('/event/checkout', [TransactionController::class, 'checkoutPreview']);
 Route::get('/event/invoice/{id}', [TransactionController::class, 'invoice']);
 Route::post('/event/checkout-proccess', [TransactionController::class, 'transaction']);
@@ -215,10 +212,10 @@ Route::get('/{event}', [EventController::class, 'show']);
 Route::get('/event/{event}', [EventController::class, 'show']);
 
 
-//Resend email
+# Resend email
 Route::post('/email/verification-notification', [AuthController::class, 'resendEmail'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
-//forgot password
+# forgot password
 Route::get('/auth/forgot-password', [AuthController::class, 'forgotPasswordView'])->middleware('guest')->name('password.request');
 Route::post('/auth/forgot-password', [AuthController::class, 'forgotPassword'])->middleware('guest')->name('password.email');
 Route::get('/auth/reset-password/{token}', [AuthController::class, 'resetPasswordView'])->middleware('guest')->name('password.reset');
