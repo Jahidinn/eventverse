4fa967342c64fc154bb1d0de986a3859c1b870d2
jahidinn <jahidin.inspirit@gmail.com>
2024-06-16 02:35:48 +0700

    Update style pricing page

--- resources/views/article/page-pricing.blade.php
+++ resources/views/article/page-pricing.blade.php
@@ -15,10 +15,10 @@
 
     <section class="container mt-0 pb-0 pt-5">
         <div class="mb-3">
-            <h3>Biaya transaksi di eventconnect.id</h3>
-            <span class="text-danger"><i>Biaya transaksi di bebankan kepada penyelenggara</i></span>
+            <h4>Biaya transaksi di eventconnect.id</h4>
+            <small class="text-danger"><i>Biaya transaksi di bebankan kepada penyelenggara</i></small>
         </div>
-        <div class="row">
+        <div class="row text-article">
             <div class="col-md-8 mb-3">
                 <div class="card mb-3">
                     <div class="card-header">
@@ -52,9 +52,13 @@
                 <a href="#simulasi-pembayaran" class="btn btn-info btn-sm">Simulasi biaya <i
                         class="fas fa-chevron-circle-right"></i></a>
                 <div class="bg-secondary p-2 mt-2 text-white">
-                    <small><i class="fas fa-caret-right"></i> Total Penjualan = Total Tiket Terjual x Harga
-                        Tiket</small><br>
-                    <small><i class="fas fa-caret-right"></i> Biaya sudah termasuk PPN 11%</small>
+                    <small>
+                        <i class="fas fa-caret-right"></i> Total Penjualan = Total Tiket Terjual x Harga Tiket
+                    </small>
+                    <br>
+                    <small>
+                        <i class="fas fa-caret-right"></i> Biaya sudah termasuk PPN 11%
+                    </small>
                 </div>
             </div>
         </div>
@@ -62,21 +66,26 @@
 
     <section class="container mt-4" id="simulasi-pembayaran">
         <div class="mb-3">
-            <h3>Simulasi biaya</h3>
+            <h4>Simulasi biaya</h4>
         </div>
         <div class="card">
+            <div class="card-body text-article">
+                <p>
+                    <i>Sebuah lembaga pendidikan <b>Smart Education</b>, membuat event “Lomba Karya Tulis Ilmiah (LKTIN)”
+                        dengan harga tiket pendaftaran <b>Rp 100.000.</b></i>
+                </p>
+                <p>
+                    <b>Case 1: </b><br>
+                    Melani membeli 5 tiket menggunakan ShopeePay, maka: Melani akan membayar sebesar Rp 500.000, Smart
+                    Education sebagai penyelenggara akan dikenakan biaya sebesar <b>3% x Total penjualan = Rp 15.000</b>
+                    Maka, jumlah uang yang didapat oleh Smart Educations adalah sebesar Rp 485.000
+                </p>
 
-            <div class="card-body">
-                <p><i>Sebuah lembaga pendidikan <b>Smart Education</b>, membuat event “Lomba Karya Tulis Ilmiah (LKTIN)”
-                        dengan harga tiket pendaftaran <b>Rp 100.000.</b></i></p>
-                <p><b>Case 1: </b><br>
-                    Melani membeli 5 tiket menggunakan ShopeePay , maka: Melani akan membayar sebesar Rp 500.000 Smart
-                    Education akan dikenakan biaya sebesar <b>3% x Total penjualan = Rp 15.000</b> Maka, jumlah uang yang
-                    didapat oleh Smart Educations adalah sebesar Rp 285.000</p>
-
-                <p><b>Case 2:</b><br>
-                    Melani membeli 5 tiket pendaftaran menggunakan Virtual Account (VA) dan akan membayar sebesar Rp 500.000
-                    Reza membeli 5 tiket menggunakan GoPay dan akan membayar sebesar Rp500.000 </p>
+                <p>
+                    <b>Case 2:</b><br>
+                    Melani membeli 5 tiket pendaftaran menggunakan Virtual Account (VA) dan akan membayar sebesar Rp
+                    500.000, Reza membeli 5 tiket menggunakan GoPay dan akan membayar sebesar Rp500.000
+                </p>
                 <p>Smart Education akan dikenakan biaya sebesar:</p>
                 <p>Tiket Melani, pembayaran menggunakan Virtual Account (VA)<br>
                     = (1,5% x Total Penjualan) + (4.500 x jumlah tiket)<br>
@@ -89,13 +98,14 @@
                     = 3% x Rp 500.000 <br>
                     = Rp15.000</p>
 
-                <p>Biaya yang dikenakan untuk pembelian tiket pendaftaran Melani, dan Reza adalah: Rp30.000 + Rp15.000 =
-                    Rp45.000 Maka, jumlah
-                    uang yang bisa di-withdraw oleh Smrt Education adalah sebesar: <b>Rp 1.000.000 - Rp 45.000 = Rp
-                        955.000</b></p>
+                <p>Biaya admin yang dikenakan untuk pembelian tiket pendaftaran Melani, dan Reza adalah: <br>
+                    Rp30.000 + Rp15.000 = Rp45.000 <br>
+                    Maka, jumlah uang yang bisa di withdraw oleh Smart Education adalah sebesar: <b>Rp 1.000.000 - Rp 45.000
+                        = Rp 955.000</b>
+                </p>
             </div>
         </div>
-        <div class="mt-4">
+        <div class="mt-4 text-article">
             Kenapa harus eventconnect? <a href="/about-us" class="text-info"> <b>Cek selengkapnya ...</b></a>
         </div>
     </section>
